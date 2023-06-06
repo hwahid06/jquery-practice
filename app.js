330f49364fe129480d929be1ee2cb8728f55996a
Hameed Wah <hamwah@Hameeds-MacBook-Air.local>
2023-06-06 10:16:22 -0700

    use array of object to get colors of food

--- app.js
+++ app.js
@@ -9,6 +9,29 @@ $(document).ready(() => {
 
     startingElement.after(siblingElement);
 
+    const favoriteFoods = $("#favorite-foods");
+    console.log(favoriteFoods)
+    favoriteFoods.css('background-color', 'purple');
+
+    const favFoods = ["pizza", "falafel", "bolani", "hot dog"]
+    const favColors = ["green", "yellow", "red", "orange"]
+        
+    const favs = [
+			{ food: 'pizza', color: 'green' },
+			{ food: 'falafel', color: 'yellow' },
+			{ food: 'bolani', color: 'red' },
+			{ food: 'hot dog', color: 'orange' },
+		]
+
+    favs.forEach(obj => {
+        let favFood = $('<div></div>')
+        .text(obj.favFood)
+        .css('background-color', 'red',obj.color );
+        favoriteFoods.after(favFood)
+    })
+   
+
+
 
 
 });
