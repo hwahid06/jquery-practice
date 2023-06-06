7c23d9228942334d95f63fc34e280ac180c5c7a4
Hameed Wah <hamwah@Hameeds-MacBook-Air.local>
2023-06-06 08:03:56 -0700

    add div, define new div in jQuery and add sibling

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 $(document).ready(() => {
-    console.log('jquery is working!');
+    //This grabs the element with ID starting point
+    const startingElement = $("#starting-point");
+    //Creates a new Div
+    const siblingElement = $("<div></div>");
+    //Adds test to the new div
+    siblingElement.text('this is the next part');
+    //appends the new div to startingElement
+
+    startingElement.after(siblingElement);
+
+
+
 });
